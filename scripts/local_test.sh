c239a031da60896142121851e8151a3b2b6ecf1e
Alex Hall <alex.mojaki@gmail.com>
2016-01-22 01:30:54 +0200

    Change hashbangs and remove mysterious dot

--- scripts/local_test.sh
+++ scripts/local_test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Used by Yola internally for testing (mainly to automatically obtain a license key)
 
 set -eux
@@ -7,4 +7,4 @@ configurator --local userservice qa
 license_key=$(python -c "print __import__('json').load(open('configuration.json'))['userservice']['geoip']['license_key']")
 rm configuration.json
 
-../virtualenv/bin/python maxmindupdater/__main__.py geoip.mmdb $license_key GeoIP2-Country
+./virtualenv/bin/python maxmindupdater/__main__.py geoip.mmdb $license_key GeoIP2-Country
