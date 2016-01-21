433200e3e67b2f8f1226a952434eeae15ce1467c
Alex Hall <alex.mojaki@gmail.com>
2016-01-21 22:55:41 +0200

    Show python 3 support in setup.py

--- setup.py
+++ setup.py
@@ -25,5 +25,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
     ],
 )
