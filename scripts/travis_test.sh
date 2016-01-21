f1735b746a72764a9093ab7fd2c478bade2a7423
Alex Hall <alex.mojaki@gmail.com>
2016-01-22 00:09:44 +0200

    Keep license key out of log

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -5,7 +5,9 @@ set -eux
 python_version=$(python --version 2>&1 | cut -d ' ' -f2)
 db_file=geoip_${python_version}.mmdb
 PYTHONPATH=`pwd`
+set +x
 python maxmindupdater/__main__.py $db_file $LICENSE_KEY GeoIP2-Country
+set -x
 
 # Ensure all python versions produce the same result (and that they actually produced a result)
 for other_db_file in geoip_*.mmdb ; do
