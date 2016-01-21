cd9b25d8da0348e541641a345f71a97fe2f9f427
Alex Hall <alex.mojaki@gmail.com>
2016-01-22 00:26:11 +0200

    Don't try to compare results of python versions, they're isolated

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -2,14 +2,11 @@
 
 set -eux
 
-python_version=$(python --version 2>&1 | cut -d ' ' -f2)
-db_file=geoip_${python_version}.mmdb
+db_file=geoip.mmdb
 export PYTHONPATH=`pwd`
 set +x
 python maxmindupdater/__main__.py $db_file $LICENSE_KEY GeoIP2-Country
 set -x
 
-# Ensure all python versions produce the same result (and that they actually produced a result)
-for other_db_file in geoip_*.mmdb ; do
-    cmp $db_file $other_db_file
-done
+# Ensure file was produced
+ls $db_file
