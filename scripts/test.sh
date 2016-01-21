6a92250f4435e3d3051e3c096ac257529ae18686
Alex Hall <alex.mojaki@gmail.com>
2016-01-21 22:51:42 +0200

    Add test script that grabs license_key from configurator

---
+++ scripts/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Used by Yola internally for testing (mainly to automatically obtain a license key)
+
+set -eux
+
+configurator --local userservice qa
+license_key=$(python -c "print __import__('json').load(open('configuration.json'))['userservice']['geoip']['license_key']")
+rm configuration.json
+
+../virtualenv/bin/python maxmindupdater/__main__.py geoip.mmdb $license_key GeoIP2-Country
