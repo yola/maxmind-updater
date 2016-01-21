b1e664dc070acf0fb7d2945836fd528eb043b215
Alex Hall <alex.mojaki@gmail.com>
2016-01-21 23:52:45 +0200

    Add travis testing

---
+++ scripts/travis_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eux
+
+python_version=$(python --version 2>&1 | cut -d ' ' -f2)
+db_file=geoip_${python_version}.mmdb
+python maxmindupdater/__main__.py $db_file $LICENSE_KEY GeoIP2-Country
+
+# Ensure all python versions produce the same result (and that they actually produced a result)
+for other_db_file in geoip_*.mmdb ; do
+    cmp $db_file $other_db_file
+done
