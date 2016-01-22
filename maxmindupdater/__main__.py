c52b14260cd7267a873951c7c83181ca9768adb1
Alex Hall <alex.mojaki@gmail.com>
2016-01-22 02:35:39 +0200

    Add descriptions to CLI arguments

--- maxmindupdater/__main__.py
+++ maxmindupdater/__main__.py
@@ -5,9 +5,12 @@ from maxmindupdater import update_db
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('db_path')
-    parser.add_argument('license_key')
-    parser.add_argument('edition_id')
+    parser.add_argument(
+        'db_path', help='Path to the maxmind database file to keep updated')
+    parser.add_argument(
+        'license_key', help='Maxmind account license key')
+    parser.add_argument(
+        'edition_id', help='Database edition ID')
     args = parser.parse_args()
     update_db(args.db_path, args.license_key, args.edition_id)
 
