f74f517a75fcf9f6858cc37823a19970cb27bd54
Alex Hall <alex.mojaki@gmail.com>
2016-01-26 11:26:50 +0200

    Include more general python classifiers for searchability

--- setup.py
+++ setup.py
@@ -30,7 +30,10 @@ setup(
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
